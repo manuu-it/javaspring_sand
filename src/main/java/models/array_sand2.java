bec31610abf87151f124a632a27cf8ffb95ed29d
manuu <matsuki0903@gmail.com>
2025-04-23 20:22:45 +0900

    array_sand

---
+++ src/main/java/models/array_sand2.java
@@ -0,0 +1,27 @@
+package models;
+
+public class array_sand2 {
+	
+//	int[2] bb = new int[]://コンパイルエラー
+//	int[][] bb = new int[][5];//コンパイルエラー 
+
+//	int[][] abc = new int[][]; //コンパイルエラー 
+
+	
+	int[][] abc ={{1},{2}}; //2行3列の二次元配列を作成
+//	
+//     // 各行の列数を後から決定
+//    abc[0] = new int[3]; // 1行目に3列
+//    abc[1] = new int[5]; // 2行目に5列
+//
+//    System.out.println(abc[0].length); // 1（1行目の列数）
+//    System.out.println(abc[1].length); // 3（2行目の列数）
+
+    
+    
+//	aa[0]=1;
+	
+	
+//	bb[0][0]=11;
+	
+}
---
+++ src/main/java/models/array_sand3.java
@@ -0,0 +1,15 @@
+package models;
+
+public class array_sand3 {
+	
+	int[] aa=new int[2];
+	
+//	aa[0]=1;
+//	aa[0]=2;
+//	aa[1]=3;
+	
+//	System.out.println(aa[0]); // 1（1行目の列数）
+//    System.out.println(aa[1]); // 3（2行目の列数）
+	
+}
+
